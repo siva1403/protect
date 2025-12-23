41702d01c04c4782231135baa7d0836d5ace24d0
siva1403 <51404790+siva1403@users.noreply.github.com>
2025-12-23 16:26:12 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -5,5 +5,5 @@ sudo apt-get install maven -y
 git clone https://github.com/NarenPrakash/thymeleafExample.git
 cd "./thymeleafExample"
 mvn clean install
-cd "./home/runner/work/protect/protect/thymeleafExample/target/"
+cd "./target/"
 java -jar *.jar
