010d755a8de681be81d9ffb5fbd46a7be23f6b8a
siva1403 <51404790+siva1403@users.noreply.github.com>
2025-12-23 14:43:38 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -3,6 +3,4 @@ sudo apt-get install git -y
 sudo apt-get install openjdk-11-jre -y 
 sudo apt-get install maven -y
 git clone https://github.com/NarenPrakash/thymeleafExample.git
-cd /thymeleafExample
-mvn clean install
-cd /thymeleafExample/target/*.jar
+
